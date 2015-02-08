674d3e54c0251300e2894683939c8ee89c3e2daf
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-02-08 12:48:34 +0500

    update

--- HMB Bluetooth Controller/AppDelegate.swift
+++ HMB Bluetooth Controller/AppDelegate.swift
@@ -6,12 +6,9 @@
 //  Copyright (c) 2015 HMB-TEC. All rights reserved.
 //
 
-<<<<<<< HEAD
 // Master Branch
-=======
-// Mohsin's Branch
 
->>>>>>> Mohsin's-Branch
+
 
 import UIKit
 import CoreData
